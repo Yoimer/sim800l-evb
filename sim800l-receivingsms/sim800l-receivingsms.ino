5225ff80f4c1ae256001ec97a96d7f23361c426c
Yoimer Roman <ydrf19@gmail.com>
2017-02-13 12:48:57 -0400

    Release v1.0. Update. It turns ON or OFF LED on pin 13 depending on SMS command. Number has to be registered on SIM in order to be accepted

--- sim800l-receivingsms/sim800l-receivingsms.ino
+++ sim800l-receivingsms/sim800l-receivingsms.ino
@@ -1,11 +1,94 @@
+/* This piece of code receives SMS in order to turn on or off LED attached on PIN 13 on Arduino UNO board using
+ SIM800L-EVB Module.
+
+ If phone number is not on phonebook (contacts) the system rejects anything from them.
+ 
+ This action is based on a specific command (LED ON) or (LED OFF) on SMS message.
+ 
+ As mentioned before it will only take that action if the number was previously saved as a contact on SIMCard. This might be done by 
+ inserting SIMcard on a mobile phone and adding the contact or via AT commands.
+ 
+ Check out the "Add contact" using AT commands code on: (Here comes the GitHub repo with that software, but ot has not been made yet)
+ This only processes national numbers (ones that don't include the +  prefix).
+ 
+ For example (AT+CPBW=1,"04161587896",129,"name") where 129 means national numbers.
+ It is IMPERATIVE to save the number with a name or alias and not to leave it empty.
+ If by mistake someone tries something like (AT+CPBW= 1,"+584161587896",129,"name") or (AT+CPBW= 1,"04161587896",145,"name")
+ it won't work at all since it has a "+" sign in the first case and the 145 integer in the second.  
+ The 129 integer is taken as a flag for data processing in future releases. Feel free to change the logic at 
+ YOU OWN RISK. KEEP THIS IN MIND PLEASE!!
+ 
+*
+*
+*
+
+ We will use the GPRS library from SEEEDUINO. It has a lot of functions for the SIM800L board that will make the development process easier and faster.
+ You can get it from https://github.com/Seeed-Studio/Seeeduino_GPRS.git
+ 
+* 
+* 
+* 
+ In case you don't know or remember how to implement a library on Arduino IDE,
+ go on https://www.arduino.cc/en/hacking/libraries. Everything is explanied there very detailed.
+ 
+*
+*
+*
+*/
+
+
+
+/****************************************************************Start of the code********************************************************/
+
+//--------------------------------Start-Libraries Section----------------------------------------------------//
+
 #include <gprs.h>
 #include <SoftwareSerial.h>
- 
+
+//--------------------------------End-Libraries Section----------------------------------------------------//
+
+
+//--------------------------------Start-Definition Section--------------------------------------------------// 
+
 #define TIMEOUT    5000
 #define LED_PIN    13
+
+//--------------------------------End-Definition Section--------------------------------------------------//
+
+
+//--------------------------------Start-Object Creation--------------------------------------------------//
  
-bool ledStatus = LOW;
 GPRS gprs;
+
+//--------------------------------End-Object Creation--------------------------------------------------//
+
+
+//--------------------------------Start-Variable Declaration-------------------------------------------//
+ 
+//Initial values of variables processing any character from serial
+
+////Variable to hold last line of serial output from SIM800
+char currentLine[500] = "";
+int currentLineIndex = 0;
+
+////Boolean to be set to true if message notificaion was found and next
+////line of serial output is the actual SMS message content
+bool nextLineIsMessage = false;
+
+//Boolean to be set to true if message has "LED ON" on its content
+bool ledStatus = LOW;
+
+//Integers value to determine whether the number who sends SMS belongs the PhoneBook entry previously set.
+int firstComma = -1;
+int secondComma = -1;
+
+//Boolean to be set to true if number is found on phonebook
+bool isInPhonebook = false;
+
+//--------------------------------End-Variable Declaration-------------------------------------------//
+
+
+//--------------------------------Start-Setup section-----------------------------------------------//
  
 void setup() {
   Serial.begin(9600);
@@ -37,24 +120,30 @@ void setup() {
   }
  
   Serial.println("Init success");
-  //ledStatus = LOW;
   Serial.println(ledStatus);
   
 }
+
+//--------------------------------End-Setup Section----------------------------------------------//
+
  
-//Variable to hold last line of serial output from SIM800
-char currentLine[500] = "";
-int currentLineIndex = 0;
- 
-//Boolean to be set to true if message notificaion was found and next
-//line of serial output is the actual SMS message content
-bool nextLineIsMessage = false;
+////Variable to hold last line of serial output from SIM800
+//char currentLine[500] = "";
+//int currentLineIndex = 0;
+// 
+////Boolean to be set to true if message notificaion was found and next
+////line of serial output is the actual SMS message content
+//bool nextLineIsMessage = false;
+//
+//
+//// Integers value to determine whether the number who sends SMS belongs the PhoneBook entry previously set.
+//int firstComma = -1;
+//int secondComma = -1;
+//
+////Boolean to be set to true if number is found on phonebook
+//bool isInPhonebook = false;
 
-
-// Integers value to determine whether the number who sends SMS belongs the PhoneBook entry previously set.
-int firstComma = -1;
-int secondComma = -1;
-
+//--------------------------------Start-Loop Section--------------------------------------------//
 
 void loop() {
   //Write current status to LED pin
@@ -72,27 +161,31 @@ void loop() {
         if(lastLine.startsWith("+CMT:")){
           
           firstComma = lastLine.indexOf(',');
-          Serial.println(firstComma);
+          Serial.println(firstComma);  //For debugging
           secondComma = lastLine.indexOf(',', firstComma + 1);
-          Serial.println(secondComma);
+          Serial.println(secondComma); //For debugging
 
           /*If secondComma position is greater than 22 it means that the number was previously entered on PhoneBook
-             When not secondComma is just 22.
+             When not; secondComma is just 22.
               
              Example:
-             +CMT: "04168262667","Yoimer","17/02/11,16:41:41-16"  secondComma position is 28
-             +CMT: "04168262667","","17/02/11,16:41:41-16"        secondComma position is 22 which means this number is not on SIMCard PhoneBook
+             +CMT: "04161587896","Yoimer","17/02/11,16:41:41-16"  secondComma position is 28
+             +CMT: "04161587896","","17/02/11,16:41:41-16"        secondComma position is 22 which means this number is not on SIMCard PhoneBook
            */
 
+          // If exists on Phonebook
           if(secondComma > 22){
-            Serial.println("In Phonebook");
+            Serial.println("In Phonebook"); //For debugging
+            isInPhonebook = true;
+            Serial.println(isInPhonebook);
           }else{
-            Serial.println("No in Phonebook");
+            Serial.println("Not in Phonebook"); //For debugging
           }
           
           Serial.println(lastLine);
           nextLineIsMessage = true;
-        } else if (lastLine.length() > 0) {
+
+        } else if ((lastLine.length() > 0) && (isInPhonebook)) {
           if(nextLineIsMessage) {
             Serial.println(lastLine);
              
@@ -104,9 +197,12 @@ void loop() {
             }
              
             nextLineIsMessage = false;
+            isInPhonebook = false;
           }
            
-        }
+        }/*else{
+          Serial.println("Not Allowed");
+        }*/
          
         //Clear char array for next line of read
         for( int i = 0; i < sizeof(currentLine);  ++i ) {
@@ -118,3 +214,9 @@ void loop() {
     }
   }
 }
+
+//--------------------------------End-Function Section-------------------------------------------//
+
+
+/****************************************************************End of the code********************************************************/
+
