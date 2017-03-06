412735a58f4efb9d23638f234dfdaf559e622839
Yoimer <ydrf19@gmail.com>
2017-03-05 21:25:05 -0400

    Modification. The whole code changed for improvement. It now shows the whole received String divided into 2 lines but using functions as Castillo suggested

--- sim800l-receivingphonecall/sim800l-receivingphonecall.ino
+++ sim800l-receivingphonecall/sim800l-receivingphonecall.ino
@@ -1,30 +1,25 @@
 /* This piece of code receives phonecalls in order to turn off LED attached on PIN 13 on Arduino UNO board using
- SIM800L-EVB Module.
+  SIM800L-EVB Module.
 
-If phone number is not on contact (contacts) the system rejects anything from them.
+  If phone number is not on contact (contacts) the system rejects anything from them.
 
-if on contact turns off LED.
+  if on contact turns off LED.
 
+  Check out how to add a contact on the SIM Card as mentioned in the Spanish version of manual
+  "Instalación de software Control de Encendido y Apagado por SMS .Prueba de Concepto del SIM800L-EVB en Arduino UNO_Finalizado"
+  on page 2 from https://drive.google.com/folderview?id=0BxtBNyHdFnkkUnl0RGt0RTFOZzQ
+  This only processes national numbers (ones that don't include the +  prefix).
 
-/OJO/ CHANGES THIS! //////////
+  For example (AT+CPBW=1,"04161587896",129,"name") where 129 means national numbers.
+  It is IMPERATIVE to save the number with a name or alias and not to leave it empty.
+  If by mistake someone tries something like (AT+CPBW= 1,"+584161587896",129,"name") or (AT+CPBW= 1,"04161587896",145,"name")
+  it won't work at all since it has a "+" sign in the first case and the 145 integer in the second.
 
-Check out the "Add contact" using AT commands code on: (Here comes the GitHub repo with that software, but ot has not been made yet)
-This only processes national numbers (ones that don't include the +  prefix).
+  We will use the GPRS library from SEEEDUINO. It has a lot of functions for the SIM800L board that will make the development process easier and faster.
+  You can get it from https://github.com/Seeed-Studio/Seeeduino_GPRS.git
 
-For example (AT+CPBW=1,"04161587896",129,"name") where 129 means national numbers.
-It is IMPERATIVE to save the number with a name or alias and not to leave it empty.
-If by mistake someone tries something like (AT+CPBW= 1,"+584161587896",129,"name") or (AT+CPBW= 1,"04161587896",145,"name")
-it won't work at all since it has a "+" sign in the first case and the 145 integer in the second.
-
-/OJO/ CHANGES THIS! //////////
-
-
-
-We will use the GPRS library from SEEEDUINO. It has a lot of functions for the SIM800L board that will make the development process easier and faster.
-You can get it from https://github.com/Seeed-Studio/Seeeduino_GPRS.git
-
-In case you don't know or remember how to implement a library on Arduino IDE,
-go on https://www.arduino.cc/en/hacking/libraries. Everything is explanied there very detailed.
+  In case you don't know or remember how to implement a library on Arduino IDE,
+  go on https://www.arduino.cc/en/hacking/libraries. Everything is explanied there very detailed.
 
 */
 
@@ -65,10 +60,6 @@ bool ledStatus = HIGH;
 char currentLine[500] = "";
 int currentLineIndex = 0;
 
-//Integers value to determine whether the number who sends SMS belongs the contact entry previously set.
-int firstComma = -1;
-int secondComma = -1;
-
 //Boolean to be set to true if number is found on contact
 bool isIncontact = false;
 
@@ -81,8 +72,27 @@ char phonenumber[13];
 // Variable to send SMS
 char *number = NULL;
 
-////Boolean to be set to true if call notificaion was found and next
-bool nextLineIsCall = false;
+////Boolean to be set to true if call notificaion was found and next line is NOT empty
+//bool nextLineIsCall = false;
+bool nextValidLineIsCall = false;
+
+// String which holds the last line read from Serial activitiy
+String lastLine = "";
+
+// Integer indexes
+int firstComma = -1;
+int secondComma = -1;
+int thirdComma = -1;
+int forthComma = -1;
+int fifthComma = -1;
+int firstQuote = -1;
+int secondQuote = -1;
+int len = -1;
+int j = -1;
+int i = -1;
+
+
+
 
 
 //--------------------------------End-Variable Declaration-------------------------------------------//
@@ -127,136 +137,195 @@ void setup() {
 //--------------------------------End-Setup Section----------------------------------------------//
 
 
-void loop() {
+//--------------------------------Start loop Section----------------------------------------------//
+
+
+void loop()
+{
   //Write current status to LED pin
   digitalWrite(LED_PIN, ledStatus);
-  static char contact[13];
-  int firstComma = -1;
-  int secondComma = -1;
-  int thirdComma = -1;
-  int forthComma = -1;
-  int fifthComma = -1;
-  int firstQuote = -1;
-  int secondQuote = -1;
-  int len = -1;
-  ////int j = 0; Carefull
-  ////char *number = NULL;
 
   //If there is serial output from SIM800
-  if (gprs.serialSIM800.available()) {
+  if (gprs.serialSIM800.available())
+  {
     char lastCharRead = gprs.serialSIM800.read();
+
     //Read each character from serial output until \r or \n is reached (which denotes end of line)
-    if (lastCharRead == '\r' || lastCharRead == '\n') {
-      String lastLine = String(currentLine);
+    if (lastCharRead == '\r' || lastCharRead == '\n')
+    {
+      endoflinereached();
+    }
 
-      //If last line read +CMT, New SMS Message Indications was received.
-      //Hence, next line is the message content.
-      if (lastLine.startsWith("RING")) {
-
-        Serial.println(lastLine);
-        nextLineIsCall = true;
-
-      } else if ((lastLine.length() > 0) && (nextLineIsCall)) {
-          Serial.println(lastLine);
-
-     
-          //+CLIP: "04168262667",129,"",0,"Yoimer",0   Format received (When registered on SIM Card)
-          //+CLIP: "04168262667",129,"",0,"",0         Format received (When not registered on SIM Card)
-          
-          // Determines whether call number is a contact
-          firstComma = lastLine.indexOf(',');
-          Serial.println(firstComma);  //For debugging
-          secondComma = lastLine.indexOf(',', firstComma + 1);
-          Serial.println(secondComma); //For debugging
-          thirdComma = lastLine.indexOf(',', secondComma + 1);
-          Serial.println(thirdComma);  //For debugging
-          forthComma = lastLine.indexOf(',', thirdComma + 1);
-          Serial.println(forthComma); //For debugging
-          fifthComma = lastLine.indexOf(',', forthComma + 1);
-          Serial.println(fifthComma); //For debuggin
-
-          // Extracts contact
-          int j = 0;
-          for (int i = forthComma+1; i < fifthComma; ++i) {
-           contact[j] = lastLine[i];
-           ++j;
-           }
-          contact[j] = '\0'; // Contact as a full string
-          
-          Serial.println(contact); //For Debugging        CONTACT HAS TO BE CLEANED LATER
-          
-          ///number = contact;  // number to send SMS later
-          
-          len = strlen(contact); //lenght of contact string
-          Serial.println(len);  // For Debugging
-          
-         // If exists on contact
-          if (len > 2) {
-           Serial.println("In contact"); //For debugging
-           isIncontact = true;
-           Serial.println(isIncontact);
-          }else {
-           Serial.println("Not in contact"); //For debugging
-           isIncontact = false;
-          }
-
-          // Get phone number
-          firstQuote = lastLine.indexOf(34); // ASCII character for quote "
-          Serial.println(firstQuote);  //For debugging
-          secondQuote = lastLine.indexOf(34, firstQuote + 1);
-          Serial.println(secondQuote); //For debugging
-
-          // Extracts phone number
-          j = 0;
-          for (int i = firstQuote+1; i < secondQuote; ++i) {
-           phonenumber[j] = lastLine[i];
-           ++j;
-           }
-          phonenumber[j] = '\0'; // phone number as a full string
-          number = phonenumber;
-          Serial.println(phonenumber); //For Debugging // PHONENUMBER HAS TO BE CLEANED LATER
-       
-          if(isIncontact){
-            ledStatus = 0;
-            digitalWrite(LED_PIN, ledStatus);
-            gprs.sendSMS(number,"LED has been turned OFF"); //define phone number and text
-       }
-          
-          
-         nextLineIsCall = false;
-         isIncontact = false;
-      }
-
-      //Clear char array for next line of read
-      for ( int i = 0; i < sizeof(currentLine);  ++i ) {
-        currentLine[i] = (char)0;
-//        contact[i] = (char)0;
-//        phonenumber[i] = (char)0;
-      }
-      currentLineIndex = 0;
-    } else {
+    else
+    {
       currentLine[currentLineIndex++] = lastCharRead;
     }
   }
 }
 
-
-//Start Sending SMS Section
-
-//       firstComma = lastLine.indexOf(34); //ASCII code for ""
-//       Serial.println(firstComma);  //For debugging
-//       secondComma = lastLine.indexOf(34, firstComma + 1);
-//      Serial.println(secondComma); //For debugging
+//      String lastLine = String(currentLine);
 //
-//       for (int i = firstComma+1; i < secondComma; ++i) {
-//        contact[j] = lastLine[i];
-//          ++j;
-//        }
-//       contact[j] = '\0';
-//       Serial.println(contact);
-//       number = contact;
+//      //If last line read +CMT, New SMS Message Indications was received.
+//      //Hence, next line is the message content.
+//      if (lastLine.startsWith("RING")) {
+//
+//        Serial.println(lastLine);
+//        nextLineIsCall = true;
+//
+//      } else if ((lastLine.length() > 0) && (nextLineIsCall)) {
+//          Serial.println(lastLine);
+//
+//
+//          //+CLIP: "04168262667",129,"",0,"Yoimer",0   Format received (When registered on SIM Card)
+//          //+CLIP: "04168262667",129,"",0,"",0         Format received (When not registered on SIM Card)
+//
+//          // Determines whether call number is a contact
+//          firstComma = lastLine.indexOf(',');
+//          Serial.println(firstComma);  //For debugging
+//          secondComma = lastLine.indexOf(',', firstComma + 1);
+//          Serial.println(secondComma); //For debugging
+//          thirdComma = lastLine.indexOf(',', secondComma + 1);
+//          Serial.println(thirdComma);  //For debugging
+//          forthComma = lastLine.indexOf(',', thirdComma + 1);
+//          Serial.println(forthComma); //For debugging
+//          fifthComma = lastLine.indexOf(',', forthComma + 1);
+//          Serial.println(fifthComma); //For debuggin
+//
+//          // Extracts contact
+//          ///int j = 0;
+//          j = 0;
+//          for (int i = forthComma+1; i < fifthComma; ++i) {
+//           contact[j] = lastLine[i];
+//           ++j;
+//           }
+//          contact[j] = '\0'; // Contact as a full string
+//
+//          Serial.println(contact); //For Debugging        CONTACT HAS TO BE CLEANED LATER
+//
+//          ///number = contact;  // number to send SMS later
+//
+//          len = strlen(contact); //lenght of contact string
+//          Serial.println(len);  // For Debugging
+//
+//         // If exists on contact
+//          if (len > 2) {
+//           Serial.println("In contact"); //For debugging
+//           isIncontact = true;
+//           Serial.println(isIncontact);
+//          }else {
+//           Serial.println("Not in contact"); //For debugging
+//           isIncontact = false;
+//          }
+//
+//          // Get phone number
+//          firstQuote = lastLine.indexOf(34); // ASCII character for quote "
+//          Serial.println(firstQuote);  //For debugging
+//          secondQuote = lastLine.indexOf(34, firstQuote + 1);
+//          Serial.println(secondQuote); //For debugging
+//
+//          // Extracts phone number
+//          j = 0;
+//          for (int i = firstQuote+1; i < secondQuote; ++i) {
+//           phonenumber[j] = lastLine[i];
+//           ++j;
+//           }
+//          phonenumber[j] = '\0'; // phone number as a full string
+//          number = phonenumber;
+//          Serial.println(phonenumber); //For Debugging // PHONENUMBER HAS TO BE CLEANED LATER
+//
+//          if(isIncontact){
+//            ledStatus = 0;
+//            digitalWrite(LED_PIN, ledStatus);
+//            gprs.sendSMS(number,"LED has been turned OFF"); //define phone number and text
+//       }
+//
+//
+//         nextLineIsCall = false;
+//         isIncontact = false;
+//      }
+//
+//      //Clear char array for next line of read
+//      for ( int i = 0; i < sizeof(currentLine);  ++i ) {
+//        currentLine[i] = (char)0;
+////        contact[i] = (char)0;
+////        phonenumber[i] = (char)0;
+//      }
+//      currentLineIndex = 0;
+//    } else {
+//      currentLine[currentLineIndex++] = lastCharRead;
+//    }
+//  }
+//}
+
+//--------------------------------End-loop Section----------------------------------------------//
 
 
-//End Sending SMS Section
+//--------------------------------Start Functions Section----------------------------------------------//
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void endoflinereached()
+{
+  lastLine = String(currentLine);
+
+  // If lastline reads RING, New Call Indication was received.
+  // Hence, the THIRD LINE is the caller information.
+
+  // The whole string will be something like this if caller IS registered on SIM Card:
+  //RING (First Line)
+  //     (Second Line is empty)
+  //+CLIP: "04168262667",129,"",0,"Yoimer",0 (Third Line)
+
+  // The whole string will be something like this if caller is NOT registered on SIM Card
+  //RING (First Line)
+  //     (Second Line is empty)
+  //+CLIP: "04168262667",129,"",0,"",0 (Third Line)
+
+  if (lastLine.startsWith("RING"))
+  {
+    Serial.println(lastLine);
+    ////nextLineIsCall = true;
+    nextValidLineIsCall = true;
+    //lastlineisRING();
+  }
+  else if ((lastLine.length() > 0) && (nextValidLineIsCall))
+  {
+    lastlineisCALL();
+  }
+  
+  //Clear char array for next line of read
+  for ( int i = 0; i < sizeof(currentLine);  ++i ) 
+  {
+     currentLine[i] = (char)0;
+  }
+  currentLineIndex = 0;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+void lastlineisCALL()
+{
+  if (nextValidLineIsCall)
+  {
+     Serial.println(lastLine);
+     nextValidLineIsCall = false;
+     //nextLineIsCall = false;
+     //isInPhonebook = false;
+  }
+
+  //Clear char array for next line of read
+  for ( int i = 0; i < sizeof(currentLine);  ++i ) 
+  {
+     currentLine[i] = (char)0;
+  }
+  currentLineIndex = 0;
+ 
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
 
 
