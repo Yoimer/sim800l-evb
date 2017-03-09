7f8e5debcba2c38fa887b7b82fa79b6d471f5866
Yoimer <ydrf19@gmail.com>
2017-03-09 11:33:52 -0400

    Test gprsconnection using SEEDUINO

--- sim800l-gprsconnection/sim800l-gprsconnection.ino
+++ sim800l-gprsconnection/sim800l-gprsconnection.ino
@@ -1,99 +1,52 @@
-/**************************************************************
- *
- * This script tries to auto-detect the baud rate
- * and allows direct AT commands access
- *
- * TinyGSM Getting Started guide:
- *   http://tiny.cc/tiny-gsm-readme
- *
- **************************************************************/
+/*
+Sketch: GPRS HTTP Test
 
-// Select your modem:
-#define TINY_GSM_MODEM_SIM800
-//#define TINY_GSM_MODEM_SIM900
-//#define TINY_GSM_MODEM_M590
-
-#include <TinyGsmClient.h>
-
-// Set serial for debug console (to the Serial Monitor, speed 115200)
-#define SerialMon Serial
-
-// Set serial for AT commands (to the module)
-// Use Hardware Serial on Mega, Leonardo, Micro
-//#define SerialAT Serial1
-
-// or Software Serial on Uno, Nano
+*/
+#include <gprs.h>
 #include <SoftwareSerial.h>
 
-//SIM800 TX is connected to Arduino D8
-#define SIM800_TX_PIN 8
- 
-//SIM800 RX is connected to Arduino D7
-#define SIM800_RX_PIN 7
+char http_cmd[] = "GET http://castillolk.com.ve/WhiteList.txt\r\n\r\n";
+char buffer[512];
 
-//Create software serial object to communicate with SIM800
-SoftwareSerial SerialAT(SIM800_TX_PIN,SIM800_RX_PIN);
- 
-//SoftwareSerial SerialAT(2, 3); // RX, TX
-
-TinyGsm modem(SerialAT);
-
-// Module baud rate
-uint32_t rate = 0; // Set to 0 for Auto-Detect
+GPRS gprs;
 
 void setup() {
-  // Set console baud rate
-  SerialMon.begin(115200);
-  delay(5000);
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("GPRS - HTTP Connection Test...");  
+  gprs.preInit();
+  while(0 != gprs.init()) {
+     delay(1000);
+     Serial.println("init error");
+  }  
+  while(!gprs.join("internet.digitel.ve","","")) {  //change "cmnet" to your own APN
+      Serial.println("gprs join network error");
+      delay(2000);
+  }
+  // successful DHCP
+  Serial.print("IP Address is ");
+  Serial.println(gprs.getIPAddress());
+  
+  Serial.println("Init success, start to connect castillo...");
+  
+  if(0 == gprs.connectTCP("http://castillolk.com.ve", 80)) {
+      Serial.println("connect castillolk.com.ve");
+  }else{
+      Serial.println("connect error");
+      while(1);
+  }
+
+  Serial.println("waiting to fetch...");
+  if(0 == gprs.sendTCPData(http_cmd))
+  {      
+    gprs.serialDebug();
+  }
+  
+//  gprs.closeTCP();
+//  gprs.shutTCP();
+//  Serial.println("close");  
 }
 
-void loop() {
-
-  if (!rate) {
-      static uint32_t rates[] = { 115200, 9600, 57600, 19200, 38400, 74400, 74880, 230400, 460800, 2400, 4800, 14400, 28800 };
-
-      SerialMon.println("Autodetecting baud rate");
-      for (unsigned i = 0; i < sizeof(rates)/sizeof(rates[0]); i++) {
-        SerialMon.print(String("Trying baud rate ") + rates[i] + "... ");
-        SerialAT.begin(rates[i]);
-        delay(10);
-        if (modem.autoBaud(1000)) {
-          rate = rates[i];
-          SerialMon.println(F("OK"));
-          break;
-        } else {
-          SerialMon.println(F("fail"));
-        }
-      }
-  }
-
-  if (!rate) {
-    SerialMon.println(F("***********************************************************"));
-    SerialMon.println(F(" Module does not respond!"));
-    SerialMon.println(F("   Check your Serial wiring"));
-    SerialMon.println(F("   Check the module is correctly powered and turned on"));
-    SerialMon.println(F("***********************************************************"));
-    delay(30000L);
-    return;
-  }
-
-  SerialAT.begin(rate);
-
-  // Access AT commands from Serial Monitor
-  SerialMon.println(F("***********************************************************"));
-  SerialMon.println(F(" You can now send AT commands"));
-  SerialMon.println(F(" Enter \"AT\" (without quotes), and you should see \"OK\""));
-  SerialMon.println(F(" If it doesn't work, select \"Both NL & CR\" in Serial Monitor"));
-  SerialMon.println(F("***********************************************************"));
-
-  while(true) {
-    if (SerialAT.available()) {
-      SerialMon.write(SerialAT.read());
-    }
-    if (SerialMon.available()) {
-      SerialAT.write(SerialMon.read());
-    }
-    delay(0);
-  }
+void loop() {    
+    
 }
-
