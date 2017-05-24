9690855ffe7762b9693e9887055e72b038d4abf9
Yoimer <ydrf19@gmail.com>
2017-05-24 13:57:18 -0400

    Removed commas counting section. Updated lines counting for SMS content processing

--- sim800l-readSMS-Libelium/sim800l-readSMS-Libelium.ino
+++ sim800l-readSMS-Libelium/sim800l-readSMS-Libelium.ino
@@ -22,17 +22,34 @@
  *  Implementation:    Luis Miguel Martí
  */
 
-#define LED 13 // led on pin 13
+
+// #define HEATING 9
+// #define COOLING 10
+
+#define LED 13
+
+//Temperature control
+//int cnt; //counter
+//float tdegrees; //
+//int sensorPin = A0; // input for the thermistor
+//int sensorValue = 0; // voltage value from voltage divider
 
 int8_t answer;
 int x;
 int onModulePin = 2;
 char SMS[200];
-////char aux_string[30];
-////char phone_number[] = "*********"; //phone number to send SMS
+char aux_string[30];
+char phone_number[] = "*********"; //phone number to send SMS
 char received[200];
 char message1[] = {"LED ON"};
 char message2[] = {"LED OFF"};
+//char message3[] = {"HEATING ON"};
+//char message4[] = {"HEATING OFF"};
+//char message5[] = {"TEMP"};
+//char message6[] = {"Temp: "};
+//char coma[] = {","};
+//char deg[] = {" Celsius degrees"};
+
 
 void setup()   {
   
@@ -65,57 +82,49 @@ void loop()
       received[i] = Serial.read();
     }
 	
-	 /* Example of SMS
-	 CMT: "+584168262667","2","17/05/09,14:14:46-16"
-     LED OFF
-	 
-	 "+584168262667"         sender number
-	  ,"2",                  name registered previously on sim, in this case the name is 2
-	  ,"",                   case when sender is not registered
-	  "17/05/09,14:14:46-16" date, hour and time zone
-	  */
-	 
-    //SMS comes after first line char ('\n') so discard all bytes until find it
-	// at the same time let's find out whether sender is registered or not
+	/* byte counter = 0;
 	
-	 byte i = 0;
-	 byte char_counter = 0;
-	 byte first_comma = 0;
-	 byte second_comma = 0;
-	 byte comma_counter = 0;
-     while (received[i] != '\n')
-	 {
-		char_counter++;
-		if(received[i] == ',')
-		{
-			comma_counter++;
-			switch(comma_counter)
-			{
-				case 1:
-				 first_comma = char_counter;
-				 Serial.println(first_comma);
-				 break;
-				case 2:
-				 second_comma = char_counter;
-				 Serial.println(second_comma);
-				 break;
-			}
-		}
-		i++;
-	 }
-	 
+	for (byte i = 0; i < 200; i++) {
+      delay(1);
+      if (char(received[i]) == '\n')
+	  {
+		  counter++;
+		  Serial.println(counter);
+	  }
+    }
+	 */
+	
+	 /* byte i = 0;
+      //SMS comes after quotes ('\n') so discard all bytes until find it
+     while (received[i] != '\n') i++;
      i++;
 	 byte z = 0;
-	 //reads sms
 	 while (char(received[i]) != '\n') {
       SMS[z] = received[i];
       Serial.print(SMS[z]);
 	  i++;
       z++;
 	 }
-	//prints space
-	Serial.println();
-	//checks SMS content
+	  */
+	
+	//SMS comes after quotes ('\n') so discard all bytes until find it
+	byte i = 0;
+	while (char(received[i]) != '\n')
+	{
+		i++;
+	}
+	//Serial.println(i);
+	i++;
+	//byte z = 47;
+	byte z = i;
+    //here start the SMS, data before this point are date and time
+    while (char(received[z]) != '\n') {
+      SMS[z - i] = received[z];
+      Serial.print(SMS[z - i]);
+      z++;
+    } 
+	
+	Serial.println(); //prints space
     byte message = checkSMS();
 
     switch (message) {
@@ -134,7 +143,7 @@ void loop()
         digitalWrite(LED, LOW);
         break;
     break;
-
+ 
     }
   }
 
