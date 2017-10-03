7a465e63821d23c5c616ffa39cc97acbb1d13996
Yoimer <ydrf19@gmail.com>
2017-10-03 12:37:33 -0400

    Added sendATcommand function based on nodemcu project

--- addcontact/addcontact.ino
+++ addcontact/addcontact.ino
@@ -1,12 +1,63 @@
 /*
-  Author: Dhanish
-  Created on: 19-08-2014
-  Company: Elementz Engineers Guild Pvt Ltd
-  Run this code only after SIM800 Module has powered and connected to a Network.
-  Please make a call to the module knowing whether the connection is established.
-  If connected a ring will be heard at the caller end
+	Recibe y procesa SMS basados en strings predefinidos
+	incluyendo una contraseña para tomar acciones de control,
+	registro y eliminación de usuarios.
+	Solo los 5 primeros contactos en el simcard
+	pueden agregar y eliminar usuarios.
+	Solo lo usuarios registrados en la "whitelist"
+	pueden tomar acciones de control.
+	Cualquier SMS distinto a los strings predefinidos
+	no serán tomados en cuenta por el sistema
+	Para control el SMS debe ser:
+	LED ON,0416, si el el número guardado en posición 1 del simcard
+	empieza por 0416
+	LED OFF,0416, si el el número guardado en posición 1 del simcard
+	empieza por 0416
+	Para registro y eliminación de usuarios:
+	Registro
+	ADD,posicion a guardar en sim,numero a guardar,
+	Ejemplo: ADD,6,04168262667,
+	DEL,posicion a borrar,
+	Ejemplo: DEL,85,
 */
 
+int8_t answer;
+bool isIncontact                          = false;
+bool isAuthorized                         = false;
+int x;
+unsigned long xprevious;
+char currentLine[500]                     = "";
+int currentLineIndex                      = 0;
+bool nextLineIsMessage                    = false;
+bool nextValidLineIsCall                  = false;
+String PhoneCallingIndex                  = "";
+String PhoneCalling                       = "";
+String OldPhoneCalling                    = "";
+String lastLine                           = "";
+String phonenum                           = "";
+int firstComma                            = -1;
+int prende                                =  0;
+int secondComma                           = -1;
+String Password                           = "";
+String indexAndName                       = "";
+String newContact                         = "";
+String trama                              = "";
+int SMSerror                                 = -1;
+int thirdComma                            = -1;
+int forthComma                            = -1;
+int fifthComma                            = -1;
+int firstQuote                            = -1;
+int secondQuote                           = -1;
+int swveces                               = 0;
+int len                                   = -1;
+int j                                     = -1;
+int i                                     = -1;
+int f                                     = -1;
+int r                                     = 0;
+bool isInPhonebook = false;
+char contact[13];
+char phone[21]; // a global buffer to hold phone number
+char message[100];
 int onModulePin = 13;
 
 void setup() {
@@ -15,127 +66,95 @@ void setup() {
   // initialize serial communications at 115200 bps:
   Serial.begin(115200);
   Serial.println("Starting...");
-  power_on();
+  // Comprueba conexión con red celular
+  while ( (sendATcommand("AT+CREG?", "+CREG: 0,1", 5000, 0) ||
+           sendATcommand("AT+CREG?", "+CREG: 0,5", 5000, 0)) == 0 );
 
-  delay(3000);
-
-  Serial.println("Connecting to the network...");
+  // Configura lectura de SMS como caracteres ASCII
+  sendATcommand("AT+CMGF=1", "OK", 5000, 0);
   
-  //while (sendATcommand2("AT+CREG?", "+CREG: 0,1", "+CREG: 0,5", 2000) == 0);
-
-   // Comprueba conexión con red celular
-  while ( (sendATcommand("AT+CREG?", "+CREG: 0,1", 5000) ||
-		  sendATcommand("AT+CREG?", "+CREG: 0,5", 5000)) == 0 );
-
+  // Configura lectura de SMS solo en el buffer serial
+  // y no guarda los SMS en la SIMcard
+  sendATcommand("AT+CNMI=1,2,0,0,0", "OK", 5000, 0);
   
-  Serial.println("Passed!...");
+  // Lee primer contacto guardado previamente en el SIMcard
+  // para ser ultilizado como contraseña en los SMS
+  sendATcommand("AT+CPBR=1,1", "OK\r\n", 5000, 1);
+  
+  Serial.println("Password:");
+  
+  // Imprime la contraseña en la consola
+  Serial.println(Password);
   
 }
 
 void loop() {
 }
-///////////////////////////////////////////////////////////////
-void power_on() {
 
+//**********************************************************
+
+// Función que comprueba conexión física con el SIM800L
+void power_on()
+{
   uint8_t answer = 0;
-
-  // checks if the module is started
-  answer = sendATcommand("AT", "OK", 2000);
+  answer = sendATcommand("AT", "OK", 5000, 0);
   if (answer == 0)
   {
-    // power on pulse
-    digitalWrite(onModulePin, HIGH);
-    delay(3000);
-    digitalWrite(onModulePin, LOW);
-
-    // waits for an answer from the module
-    while (answer == 0) {   // Send AT every two seconds and wait for the answer
-      answer = sendATcommand("AT", "OK", 2000);
+    while (answer == 0)
+    {
+      answer = sendATcommand("AT", "OK", 2000, 0);
     }
   }
-
 }
-/////////////////////////////////////////////////////////////////////////
-int8_t sendATcommand(char* ATcommand, char* expected_answer, unsigned int timeout) {
 
+//**********************************************************
+
+// Función que envía comandos AT al SIM800L
+// Cuando int xpassword tiene el valor de 0 no consulta contraseña
+// Cuando int xpassword tiene el valor de 1 consulta contraseña
+
+int8_t sendATcommand(char* ATcommand, char* expected_answer, unsigned int timeout, int xpassword)
+{
   uint8_t x = 0,  answer = 0;
   char response[100];
   unsigned long previous;
-
-  memset(response, '\0', 100);    // Initialize the string
-
+  memset(response, '\0', 100);
   delay(100);
-
-  //while ( Serial.available() > 0) Serial.read();   // Clean the input buffer
-
-  while (Serial.available()) { //Cleans the input buffer
+  while (Serial.available())
+  {
     Serial.read();
   }
-
-  Serial.println(ATcommand);    // Send the AT command
-
-
+  Serial.println(ATcommand);
   x = 0;
   previous = millis();
-
-  // this loop waits for the answer
-  do {
-    if (Serial.available() != 0) {
-      // if there are data in the UART input buffer, reads it and checks for the asnwer
+  do
+  {
+    if (Serial.available() != 0)
+    {
       response[x] = Serial.read();
       x++;
-      // check if the desired answer  is in the response of the module
       if (strstr(response, expected_answer) != NULL)
       {
         answer = 1;
+        String numbFromSim = String(response);
+        numbFromSim = numbFromSim.substring(numbFromSim.indexOf(":"),
+                                            numbFromSim.indexOf(",129,"));
+        numbFromSim = numbFromSim.substring((numbFromSim.indexOf(34) + 1),
+                                            numbFromSim.indexOf(34, numbFromSim.indexOf(34) + 1));
+        if ( xpassword == 1)
+        {
+          numbFromSim = numbFromSim.substring( 0, 4);
+          Password = numbFromSim ;
+          return 0;
+        }
+        else
+        {
+          numbFromSim = numbFromSim.substring( 0, 11 );
+        }
       }
     }
-    // Waits for the asnwer with time out
-  } while ((answer == 0) && ((millis() - previous) < timeout));
 
-  return answer;
-}
-//////////////////////////////////////////////////////////////////////////////////////////
-int8_t sendATcommand2(char* ATcommand, char* expected_answer1, char* expected_answer2, unsigned int timeout) {
-
-  uint8_t x = 0,  answer = 0;
-  char response[100];
-  unsigned long previous;
-
-  memset(response, '\0', 100);    // Initialize the string
-
-  delay(100);
-
-  //while ( Serial.available() > 0) Serial.read();   // Clean the input buffer
-  
-  while (Serial.available()) { //Cleans the input buffer
-    Serial.read();
   }
-
-  Serial.println(ATcommand);    // Send the AT command
-
-
-  x = 0;
-  previous = millis();
-
-  // this loop waits for the answer
-  do {
-    if (Serial.available() != 0) {
-      response[x] = Serial.read();
-      x++;
-      // check if the desired answer 1 is in the response of the module
-      if (strstr(response, expected_answer1) != NULL)
-      {
-        answer = 1;
-      }
-      // check if the desired answer 2 is in the response of the module
-      if (strstr(response, expected_answer2) != NULL)
-      {
-        answer = 2;
-      }
-    }
-    // Waits for the asnwer with time out
-  } while ((answer == 0) && ((millis() - previous) < timeout));
-
+  while ((answer == 0) && ((millis() - previous) < timeout));
   return answer;
 }
