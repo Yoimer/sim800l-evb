bcaa7516afa916a9dbae99f631db6079be5974ba
Yoimer <ydrf19@gmail.com>
2018-01-17 14:26:44 -0400

    added CheckUltrasoundSensor

--- movement-alarm/movement-alarm.ino
+++ movement-alarm/movement-alarm.ino
@@ -21,22 +21,31 @@ Serial.begin(9600); // Starts the serial communication
 }
 
 void loop() {
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
 
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
+    // check movement
+    CheckUltrasoundSensor();
 
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
+}
 
-// Calculating the distance
-distance= (duration*0.034)/(2);
+////////////////////////////////////////////////////
+void CheckUltrasoundSensor()
+{
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
 
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+
+    // Calculating the distance
+    distance= (duration*0.034)/(2);
+
+    // Prints the distance on the Serial Monitor
+    Serial.print("Distance is: ");
+    Serial.println(distance);
 }
